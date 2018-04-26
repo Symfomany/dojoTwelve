9cc15e8a9e6e4784564f42e8dd4905b764409120
Symfomany <zuzu38080@gmail.com>
2018-04-26 10:34:35 +0200

    add changes

--- one.js
+++ one.js
@@ -1,5 +1,16 @@
 function accum(chaine) {
-  // you code here...
+  let reg1 = new RegExp(`[a-z]`, "i");
+  let filtreChaine = [...chaine.toLowerCase()].filter(x => reg1.test(x));
+  let calcul = "";
+  filtreChaine.forEach((element, index) => {
+    calcul = calcul + element.toUpperCase() + element.repeat(index) + "-";
+  });
+
+  let tiret = calcul.substr(0, calcul.length - 1);
+  console.log(tiret);
+
+  //console.log(filtreChaine);
 }
 
-accum("abcd");
+let test = accum("§cwA-t__");
+// console.log(test);
